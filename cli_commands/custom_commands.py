f40b8490f7c5aeb124152b733966e663730d87e5
atari-monk <atari.monk1@gmail.com>
2024-11-25 22:55:46 +0100

    argparse example

--- cli_commands/custom_commands.py
+++ cli_commands/custom_commands.py
@@ -3,17 +3,23 @@
 from cli_commands.config import LOGGER_CONFIG
 from convert.video_to_mp3 import VideoToMp3
 from cli_logger.logger import setup_logger
+from example.argparse import argparse_command
 
 logger = setup_logger(__name__, LOGGER_CONFIG)
 
 def load():
-    logger.debug("custom_commands.load() called")
-
     def vidmp3(args):
-        logger.info(f"Video to mp3, Args received: {args}")
         converter = VideoToMp3()
         converter.run(args)
 
+    def ping(_):
+        logger.info('ping')
+
+    def argparse(args):
+        argparse_command(args)
+
     return {
-        "vidmp3": vidmp3
+        "vidmp3": vidmp3,
+        "ping": ping,
+        "argparse": argparse
     }
