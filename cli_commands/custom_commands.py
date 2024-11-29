1175f1691a1946a29eae9a8578f478cc005b2e59
atari-monk <atari.monk1@gmail.com>
2024-11-29 15:11:49 +0100

    job search

--- cli_commands/custom_commands.py
+++ cli_commands/custom_commands.py
@@ -1,5 +1,6 @@
 # custom_commands.py
 
+from job_search.job_search_command import JobSearchCommand
 from scenes.read_scenes_command import ReadScenesCommand
 from shared.config import LOGGER_CONFIG
 from cli_logger.logger import setup_logger
@@ -19,5 +20,6 @@ def load():
         "logtest": logTest,
         "estimate": estimateTask,
         "report": reportTask,
-        "read_scenes": ReadScenesCommand().run
+        "read_scenes": ReadScenesCommand().run,
+        "job_search": JobSearchCommand().run,
     }
