10bf9382b17d10fbbbf455253c8e1d45c15013c2
atari-monk <atari.monk1@gmail.com>
2024-11-30 20:54:25 +0100

    pip ver 0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cli_tool_commands",
+    name="atari-monk-cli-tool-commands",
     version="0.1.0",
     packages=find_packages(),
     entry_points={
@@ -9,7 +9,7 @@ setup(
             "custom_commands = cli_commands.custom_commands:load",
         ],
     },
-    install_requires=["yt-dlp>=2023.3.1",],
+    install_requires=["yt-dlp>=2023.3.1","pyperclip", "atari-monk-keyval-storage", "atari-monk-pytoolbox", "atari-monk-cli-logger", ""],
     description="A custom commands package for CLI Tool",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
